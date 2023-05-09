b6c7c978217bdbdcdad34169f8654b60dac05e54
khushil <119103821+khushil04@users.noreply.github.com>
2023-05-10 01:12:44 +0530

    Add files via upload

---
+++ mutiplication tables.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x, n=1;
+	printf("enter a number whose table is to be printed:");
+	scanf("%d",&x);
+	for (;n<=10;){
+		printf("%d*%d = %d\n",x,n,x*n);
+		n++;
+	}
+			
+	
+	
+		
+	
+}
